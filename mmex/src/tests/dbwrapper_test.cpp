5d47cfa2aa8b880b00255666edba52f5b4924fb3
kkkvadim <kkkvadim@7f5d68d0-c712-0410-839f-9b116a576cc5>
2009-10-16 14:14:14 +0000

    -#include <wx/stdpaths.h>
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@609 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/tests/dbwrapper_test.cpp
+++ mmex/src/tests/dbwrapper_test.cpp
@@ -23,7 +23,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <dbwrapper.h>
 #include "utils.h"
 //----------------------------------------------------------------------------
-#include <wx/stdpaths.h>
 #include <wx/filename.h>
 //----------------------------------------------------------------------------
 
