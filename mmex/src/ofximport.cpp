ae79fdfef211febe02360548c409d1d79cff99ef
ellisgen <ellisgen@7f5d68d0-c712-0410-839f-9b116a576cc5>
2009-11-13 04:29:54 +0000

    Cited gnucash for original source
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@766 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/ofximport.cpp
+++ mmex/src/ofximport.cpp
@@ -1,11 +1,28 @@
-/*
- *  ofximport.cpp
- *  moneymanagerex
- *
- *  Created by Wesley Ellis on 11/12/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
+/*******************************************************************\
+ *  ofximport.cpp													*
+ *  moneymanagerex													*
+ *																	*
+ *  Adapted from gnucash source, many thanks to the gnucash team.   *
+ *  Adapted by Wesley Ellis on 11/12/09.							*
+ *																	*
+ *																	*
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       *
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org                   *
+ \*******************************************************************/
 
 #include "ofximport.h"
 #include <libofx/libofx.h>
