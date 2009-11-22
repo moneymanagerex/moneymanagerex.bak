6bf9e48cea1cd1f7514bec933934513a610fc9a4
kkkvadim <kkkvadim@7f5d68d0-c712-0410-839f-9b116a576cc5>
2009-11-22 15:31:37 +0000

    new
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@803 7f5d68d0-c712-0410-839f-9b116a576cc5

---
+++ mmex/src/paths.h
@@ -0,0 +1,53 @@
+/*******************************************************
+Copyright (C) 2009 VaDiM
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ********************************************************/
+
+#ifndef _MM_EX_PATHS_H_
+#define _MM_EX_PATHS_H_
+//----------------------------------------------------------------------------
+/*
+        mmex's files and dirs locations.
+        All functions return absolute paths.
+*/
+//----------------------------------------------------------------------------
+class wxString;
+class wxIcon;
+//----------------------------------------------------------------------------
+
+namespace mmex
+{
+
+enum EDocFile { F_README, F_CONTRIB, F_LICENSE, F_VERSION, HTML_INDEX, DOC_FILES_MAX };
+wxString getPathDoc(EDocFile f);
+
+enum EResFile { PROGRAM_ICON, SPLASH_ICON, TRANS_SOUND, RES_FILES_MAX };
+wxString getPathResource(EResFile f);
+
+// use instead of getPathResource(PROGRAM_ICON)
+const wxIcon& getProgramIcon();
+
+enum ESharedFile { CURRENCY_DB_SEED, LANG_DIR, SHARED_FILES_MAX };
+wxString getPathShared(ESharedFile f);
+
+enum EUserFile { SETTINGS, USER_FILES_MAX };
+wxString getPathUser(EUserFile f);
+
+} // namespace mmex
+
+//----------------------------------------------------------------------------
+#endif // _MM_EX_PATHS_H_
+//----------------------------------------------------------------------------
