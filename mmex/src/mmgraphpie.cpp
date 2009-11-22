08aa2008a89a99f61fde52665bcf7b424e8cc553
kkkvadim <kkkvadim@7f5d68d0-c712-0410-839f-9b116a576cc5>
2009-11-22 15:33:22 +0000

    new
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@804 7f5d68d0-c712-0410-839f-9b116a576cc5

---
+++ mmex/src/mmgraphpie.cpp
@@ -0,0 +1,36 @@
+#include "mmgraphpie.h"
+#include "chart.h"
+#include "util.h"
+
+bool sortValueList(const ValuePair& elem1, const ValuePair& elem2)
+{
+    return fabs(elem1.amount) > fabs(elem2.amount);
+}
+
+mmGraphPie::mmGraphPie() : chart(new PieChart(480, 320))
+{
+}
+
+mmGraphPie::~mmGraphPie()
+{
+}
+
+void mmGraphPie::init(std::vector<ValuePair>& valueList)
+{
+        std::vector<ChartData> pieData;
+        std::sort(valueList.begin(), valueList.end(), sortValueList);
+
+	for (size_t i = 0; i < valueList.size(); ++i) {
+                if (i < 15)
+	              pieData.push_back(ChartData(valueList[i].label, valueList[i].amount));
+	}
+
+	chart->SetData(pieData);
+	chart->Init(240, CHART_LEGEND_FIXED, PIE_CHART_ABSOLUT);
+}
+
+bool mmGraphPie::Generate(const wxString& chartTitle)
+{
+        chart->Render(chartTitle);
+	return chart->Save(GetOutputFileName());
+}
