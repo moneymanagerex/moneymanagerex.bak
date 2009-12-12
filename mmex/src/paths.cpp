fb6d0a5aef919b6e356c7a80ee57e4051dae29f4
kkkvadim <kkkvadim@7f5d68d0-c712-0410-839f-9b116a576cc5>
2009-12-12 15:07:45 +0000

    comments
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@876 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/paths.cpp
+++ mmex/src/paths.cpp
@@ -51,16 +51,9 @@ wxFileName getSettingsPathPortable()
 
         MMEX is a portable application which means ability to to run
         without installation, for example, from USB flash drive.
-        
-        To make MMEX portable
 
-        1.On Windows (assume F:\ is USB flash drive)
-          copy "C:\Program Files\MoneyManagerEx" F:\
-          copy %APPDATA%\MoneyManagerEx\mmexini.db3 F:\MoneyManagerEx
-
-        2.On Unix (assume /media/disk is mounted USB flash drive)
-          make install prefix=/media/disk
-          cp ~/.mmex/mmexini.db3 /media/disk/mmex/share/mmex
+        If mmex finds mmexini.db3 in its folder, it assumes portable 
+        mode and GetUserDir() in such case points to that folder.
 
         FIXME: security issue - temp files will be created on host filesystem.
 */
