53aae8d61cfc1d6d3b2ddaf042adf6fcaa081d2e
kkkvadim <kkkvadim@7f5d68d0-c712-0410-839f-9b116a576cc5>
2009-12-31 15:46:41 +0000

    float getFrequency(float vmax) refactoring and bugfix for huge amounts
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@890 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/chart.h
+++ mmex/src/chart.h
@@ -9,17 +9,17 @@
 class ChartData
 {
 public:
-	ChartData(const wxString &aKey, float aVal = 0);
-	ChartData(const wxString &aKey, const std::vector<float> &aSerie);
-	
-	wxString key;
-	
-	// normalized val
-	float val;
-	// real val
-	float aval;
-	
-	std::vector<float> serie;
+        ChartData( const wxString &aKey, float aVal = 0 );
+        ChartData( const wxString &aKey, const std::vector<float> &aSerie );
+
+        wxString key;
+
+        // normalized val
+        float val;
+        // real val
+        float aval;
+
+        std::vector<float> serie;
 };
 //----------------------------------------------------------------------------
 enum EChartLegend { CHART_LEGEND_FIXED, CHART_LEGEND_FLOAT };
@@ -28,41 +28,39 @@ enum EChartLegend { CHART_LEGEND_FIXED, CHART_LEGEND_FLOAT };
 class AbstractChart
 {
 public:
-	AbstractChart(int aWidth, int aHeight);
+        AbstractChart( int aWidth, int aHeight );
 
-	void SetBackground(const wxColour& colour);
-	void SetFillColour(const wxColour& colour, int style = wxSOLID);
-	void SetStrokeColour(const wxColour& colour, int width = 1, int style = wxSOLID);
+        void SetBackground( const wxColour& colour );
+        void SetFillColour( const wxColour& colour, int style = wxSOLID );
+        void SetStrokeColour( const wxColour& colour, int width = 1, int style = wxSOLID );
 
-    void ClearFillColour();
-	void ClearStrokeColour();
+        void ClearFillColour();
+        void ClearStrokeColour();
 
-    bool Save(const wxString& file);
+        bool Save( const wxString& file );
 
-    void SetData(const std::vector<ChartData> &aData, bool simpleValue = true);
-	void SetData(const std::vector<ChartData> &aData, const std::vector<wxString> &aSerieLabel);
+        void SetData( const std::vector<ChartData> &aData, bool simpleValue = true );
+        void SetData( const std::vector<ChartData> &aData, const std::vector<wxString> &aSerieLabel );
 
-    virtual bool Render(const wxString& title) = 0;
+        virtual bool Render( const wxString& title ) = 0;
 
 protected:
 
-	int GetFrequency(int j);
+        std::vector<ChartData> data;
+        std::vector<wxString> serieLabel;
 
-    std::vector<ChartData> data;
-	std::vector<wxString> serieLabel;
-	
-    enum { PAL_MAX = 24 };
-    wxColour palete[PAL_MAX];
-	
-	wxMemoryDC dc;
-	int width;
-	int height;
+        enum { PAL_MAX = 24 };
+        wxColour palete[PAL_MAX];
 
-	wxFont plainFont;
-	wxFont boldFont;
-	
+        wxMemoryDC dc;
+        int width;
+        int height;
+
+        wxFont plainFont;
+        wxFont boldFont;
+
 private:
-	wxBitmap image;
+        wxBitmap image;
 };
 //----------------------------------------------------------------------------
 enum EPieChartStyle { PIE_CHART_PERCENT, PIE_CHART_ABSOLUT };
@@ -71,15 +69,15 @@ enum EPieChartStyle { PIE_CHART_PERCENT, PIE_CHART_ABSOLUT };
 class PieChart : public AbstractChart
 {
 public:
-	PieChart(int aWidth, int aHeight);
-	
-	void Init(int aChartsize, EChartLegend aLegendMode = CHART_LEGEND_FIXED, EPieChartStyle aPieMode = PIE_CHART_PERCENT);
-	bool Render(const wxString& title = wxGetEmptyString());
+        PieChart( int aWidth, int aHeight );
+
+        void Init( int aChartsize, EChartLegend aLegendMode = CHART_LEGEND_FIXED, EPieChartStyle aPieMode = PIE_CHART_PERCENT );
+        bool Render( const wxString& title = wxGetEmptyString() );
 
 private:
-	int csize;
-	int keymode;
-	int mode;
+        int csize;
+        int keymode;
+        int mode;
 };
 //----------------------------------------------------------------------------
 enum EBarChartStyle { BAR_CHART_SIMPLE, BAR_CHART_SERIES };
@@ -88,15 +86,15 @@ enum EBarChartStyle { BAR_CHART_SIMPLE, BAR_CHART_SERIES };
 class BarChart : public AbstractChart
 {
 public:
-	BarChart(int aWidth, int aHeight);
+        BarChart( int aWidth, int aHeight );
 
-    void Init(int aChartsize, EChartLegend aLegendMode = CHART_LEGEND_FIXED, EBarChartStyle aMode = BAR_CHART_SIMPLE);
-	bool Render(const wxString& title = wxGetEmptyString());
+        void Init( int aChartsize, EChartLegend aLegendMode = CHART_LEGEND_FIXED, EBarChartStyle aMode = BAR_CHART_SIMPLE );
+        bool Render( const wxString& title = wxGetEmptyString() );
 
 private:
-	int csize;
-	int keymode;
-	int mode;
+        int csize;
+        int keymode;
+        int mode;
 };
 //----------------------------------------------------------------------------
 #endif // _MM_EX_CHART_H_
--- mmex/src/mmgraphtopcategories.cpp
+++ mmex/src/mmgraphtopcategories.cpp
@@ -18,20 +18,18 @@ mmGraphTopCategories::~mmGraphTopCategories()
 
 void mmGraphTopCategories::init(const std::vector<CategInfo> &categList)
 {
-	std::vector<ChartData> barData;
-	barData.reserve(categList.size());
+        const size_t max_cnt = std::min(categList.size(), size_t(10));
 
-	for (size_t i = 0; i < categList.size(); ++i) 
-	{
-		if (i >= 10) //Show top 10 or less
-            break;
-		if (categList[i].amount < 0.0 ) //Show withdrawal only
-		{
-        barData.push_back(ChartData(categList[i].categ, -1*(categList[i].amount)));
-		}
+	std::vector<ChartData> barData;
+	barData.reserve(max_cnt);
+
+	for (size_t i = 0; i < max_cnt; ++i) {
+
+		if (categList[i].amount < 0) // show withdrawal only
+                        barData.push_back(ChartData(categList[i].categ, -categList[i].amount));
 	}
 
-    m_chart->SetData(barData);
+        m_chart->SetData(barData);
 	m_chart->Init(140, CHART_LEGEND_FIXED, BAR_CHART_SIMPLE);
 }
 //----------------------------------------------------------------------------
