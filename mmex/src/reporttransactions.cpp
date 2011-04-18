5ec1441c235025ce7fd371950adafcbadf9ba848
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2011-04-18 10:45:38 +0000

    Code tidy up - no real changes
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@1240 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/reporttransactions.cpp
+++ mmex/src/reporttransactions.cpp
@@ -29,110 +29,122 @@
 
 #include <algorithm>
 
-mmReportTransactions::mmReportTransactions(std::vector< boost::shared_ptr<mmBankTransaction> >* trans, mmCoreDB* core) : 
-        trans_(trans), 
-        core_(core)
+mmReportTransactions::mmReportTransactions(std::vector< boost::shared_ptr<mmBankTransaction> >* trans, mmCoreDB* core)
+:trans_(trans), core_(core)
 {
 }
 
 mmReportTransactions::~mmReportTransactions()
 {
-        delete trans_;
+    delete trans_;
 }
 
 wxString mmReportTransactions::getHTMLText()
 {
-        mmHTMLBuilder hb;
-        hb.init();
-        hb.addHeader(3, _("Transaction List"));
+    mmHTMLBuilder hb;
+    hb.init();
+    hb.addHeader(3, _("Transaction List"));
 
-        wxDateTime now = wxDateTime::Now();
-        wxString dt = _("Today's Date: ") + mmGetNiceDateString(now);
-        hb.addHeader(7, dt);
-        hb.addLineBreak();
-        hb.addLineBreak();
-     
-		hb.startCenter();
+    wxDateTime now = wxDateTime::Now();
+    wxString dt = _("Today's Date: ") + mmGetNiceDateString(now);
+    hb.addHeader(7, dt);
+    hb.addLineBreak();
+    hb.addLineBreak();
+    
+    hb.startCenter();
+    hb.startTable();
 
-        hb.startTable();
+    // Display the data Headings
+    hb.startTableRow();
+    hb.addTableHeaderCell(_("Date"));
+    hb.addTableHeaderCell(_("Number"));
+    hb.addTableHeaderCell(_("Account"));
+    hb.addTableHeaderCell(_("Payee"));
+    hb.addTableHeaderCell(_("Type"));
+    hb.addTableHeaderCell(_("Category"));
+    hb.addTableHeaderCell(_("Notes"));
+    hb.addTableHeaderCell(_("Status"));
+    hb.addTableHeaderCell(_("Amount"));
+    hb.endTableRow();
 
-		hb.startTableRow();
-		hb.addTableHeaderCell(_("Date"));
-		hb.addTableHeaderCell(_("Number"));
-		hb.addTableHeaderCell(_("Account"));
-		hb.addTableHeaderCell(_("Payee"));
-		hb.addTableHeaderCell(_("Type"));
-		hb.addTableHeaderCell(_("Category"));
-        hb.addTableHeaderCell(_("Notes"));
-		hb.addTableHeaderCell(_("Status"));
-		hb.addTableHeaderCell(_("Amount"));
-		hb.endTableRow();
+    // Display the data for each row
+    bool hasTransferTransaction = false;
+    double total = 0;
+    for (unsigned int index = 0; index < trans_->size(); index++)
+    {
+        std::vector<wxString> data;
+        std::vector<boost::shared_ptr<mmBankTransaction> >& refTrans = *trans_;
+    
+        // Display the data for the selected row
+        hb.startTableRow();
+        hb.addTableCell(refTrans[index]->dateStr_);
+        hb.addTableCell(refTrans[index]->transNum_);
+        hb.addTableCell(refTrans[index]->fromAccountStr_, false, true);
+        hb.addTableCell(refTrans[index]->payeeStr_, false, true);
+        if (refTrans[index]->transType_ == wxT("Deposit"))
+            hb.addTableCell(_("Deposit"));
+        else if (refTrans[index]->transType_ == wxT("Withdrawal"))
+            hb.addTableCell(_("Withdrawal"));
+        else if (refTrans[index]->transType_ == wxT("Transfer"))
+            hb.addTableCell(_("Transfer"));
+        hb.addTableCell(refTrans[index]->fullCatStr_, false, true);
+        hb.addTableCell(refTrans[index]->notes_, false, true);
+        hb.addTableCell(refTrans[index]->status_);
 
-        double total = 0;
-        for (unsigned int index = 0; index < trans_->size(); index++)
+        if (refTrans[index]->reportCategAmountStr_ == wxT(""))  
+            hb.addTableCell(refTrans[index]->transAmtString_, true);
+        else
+            hb.addTableCell(refTrans[index]->reportCategAmountStr_, true);
+        
+        hb.endTableRow();
+
+        // Get the exchange rate for the selected account
+        double dbRate = core_->accountList_.getAccountBaseCurrencyConvRate(refTrans[index]->accountID_);
+        double transAmount = refTrans[index]->amt_ * dbRate;
+        if (refTrans[index]->reportCategAmountStr_ != wxT(""))
+            transAmount = refTrans[index]->reportCategAmount_ * dbRate;
+
+        if (refTrans[index]->transType_ == wxT("Deposit"))
         {
-            std::vector<wxString> data;
-            std::vector<boost::shared_ptr<mmBankTransaction> >& refTrans = *trans_;
-
-			hb.startTableRow();
-			hb.addTableCell(refTrans[index]->dateStr_);
-			hb.addTableCell(refTrans[index]->transNum_);
-			hb.addTableCell(refTrans[index]->fromAccountStr_, false, true);
-			hb.addTableCell(refTrans[index]->payeeStr_, false, true);
-			if (refTrans[index]->transType_ == wxT("Deposit"))
-				hb.addTableCell(_("Deposit"));
-			else if (refTrans[index]->transType_ == wxT("Withdrawal"))
-				hb.addTableCell(_("Withdrawal"));
-			else if (refTrans[index]->transType_ == wxT("Transfer"))
-				hb.addTableCell(_("Transfer"));
-			hb.addTableCell(refTrans[index]->fullCatStr_, false, true);
-            hb.addTableCell(refTrans[index]->notes_, false, true);
-			hb.addTableCell(refTrans[index]->status_);
-
-            if (refTrans[index]->reportCategAmountStr_ == wxT(""))  
-    			hb.addTableCell(refTrans[index]->transAmtString_, true);
-            else
-            {
-                hb.addTableCell(refTrans[index]->reportCategAmountStr_, true);
-            }
-			hb.endTableRow();
-
-            double dbRate = core_->accountList_.getAccountBaseCurrencyConvRate(refTrans[index]->accountID_);
-            double transAmount = refTrans[index]->amt_ * dbRate;
-            if (refTrans[index]->reportCategAmountStr_ != wxT(""))
-                transAmount = refTrans[index]->reportCategAmount_ * dbRate;
-
-            if (refTrans[index]->transType_ == wxT("Deposit"))
-            {
-                total += transAmount;
-            }
-            else if (refTrans[index]->transType_ == wxT("Withdrawal"))
-            {
-                total -= transAmount;
-            }
-            else if (refTrans[index]->transType_ == wxT("Transfer"))
-            {
-                total += transAmount;
-            }
+            total += transAmount;
         }
+        else if (refTrans[index]->transType_ == wxT("Withdrawal"))
+        {
+            total -= transAmount;
+        }
+        else if (refTrans[index]->transType_ == wxT("Transfer"))
+        {
+            total += transAmount;
+            hasTransferTransaction = true;
+        }
+    }
 
-        wxString balanceStr;
-        core_->currencyList_.loadBaseCurrencySettings();
-        mmex::formatDoubleToCurrency(total, balanceStr);
+    // work out the total balance for all the data at base rate
+    wxString balanceStr;
+    core_->currencyList_.loadBaseCurrencySettings();
+    mmex::formatDoubleToCurrency(total, balanceStr);
 
+    // display the total balance.
 	hb.addRowSeparator(9);
 	hb.addTotalRow(_("Total Amount: "), 9, balanceStr);
-        hb.endTable();
 
+    hb.endTable();
 	hb.endCenter();
-        hb.end();
+
+    if (hasTransferTransaction)
+    {
+        hb.addHorizontalLine();
+        hb.addHeader(7, _("<b>Note:</b> Total Amount may be incorrect as <b>transfers</B> have been added to the total."));
+    }
+    hb.end();
+
 
 //TODO: We want to know what parameters have been selected
-        //dt = _("Filter: ");
+    //dt = _("Filter: ");
 
-        //hb.addLineBreak();
-        //hb.addLineBreak();
-        //hb.addHeader(7, dt);
+    //hb.addLineBreak();
+    //hb.addLineBreak();
+    //hb.addHeader(7, dt);
 
-        return hb.getHTMLText();
+    return hb.getHTMLText();
 }
