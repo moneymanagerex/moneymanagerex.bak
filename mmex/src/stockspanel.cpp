42b754fad087606221a8b27f2e17a11aa7a70a59
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2011-05-27 14:47:06 +0000

    some objects geometry a bit fixed
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@1296 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/stockspanel.cpp
+++ mmex/src/stockspanel.cpp
@@ -192,7 +192,7 @@ void mmStocksPanel::CreateControls()
 
     wxStaticText* itemStaticText10 = new wxStaticText( headerPanel,
                                      ID_PANEL_CHECKING_STATIC_BALHEADER,
-                                     _("Total:"), wxDefaultPosition, wxSize(500, 20), 0 );
+                                     _("Total:"), wxDefaultPosition, wxSize(800, 20), 0 );
 
     m_LED = new awxLed( headerPanel, ID_PANEL_STOCK_UPDATE_LED, wxDefaultPosition, wxDefaultSize, awxLED_GREEN, 1, 5 );
     //m_LED = new awxLed( headerPanel, ID_PANEL_STOCK_UPDATE_LED, wxDefaultPosition, wxDefaultSize, awxLED_GREEN);
@@ -296,39 +296,39 @@ void mmStocksPanel::CreateControls()
     wxButton* itemButton6 = new wxButton( itemPanel12, ID_BUTTON_NEW_STOCK, _("&New"));
     itemButton6->SetForegroundColour(wxColour(wxT("FOREST GREEN")));
     itemButton6->SetToolTip(_("New Stock Investment"));
-    itemBoxSizer5->Add(itemButton6, 0, wxGROW|wxALL, 1);
+    itemBoxSizer5->Add(itemButton6, 0, wxALIGN_CENTER_VERTICAL|wxALL, 4);
 
     wxButton* itemButton81 = new wxButton( itemPanel12, ID_BUTTON_EDIT_STOCK, _("&Edit"));
     itemButton81->SetForegroundColour(wxColour(wxT("ORANGE")));
     itemButton81->SetToolTip(_("Edit Stock Investment"));
-    itemBoxSizer5->Add(itemButton81, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    itemBoxSizer5->Add(itemButton81, 0, wxALIGN_CENTER_VERTICAL|wxALL, 4);
 	itemButton81->Enable(false);
 
     wxButton* itemButton7 = new wxButton( itemPanel12, ID_BUTTON_DELETE_STOCK, _("&Delete"));
     itemButton7->SetForegroundColour(wxColour(wxT("RED")));
     itemButton7->SetToolTip(_("Delete Stock Investment"));
-    itemBoxSizer5->Add(itemButton7, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    itemBoxSizer5->Add(itemButton7, 0, wxALIGN_CENTER_VERTICAL|wxALL, 4);
 	itemButton7->Enable(false);
 	
     wxButton* itemButton8 = new wxButton( itemPanel12, ID_BUTTON_REFRESH_STOCK, _("&Refresh"),
                                           wxDefaultPosition, wxDefaultSize, 0 );
     itemButton8->SetToolTip(_("Refresh Stock Prices from Yahoo"));
-    itemBoxSizer5->Add(itemButton8, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    itemBoxSizer5->Add(itemButton8, 0, wxALIGN_CENTER_VERTICAL|wxALL, 4);
 
     wxButton* itemButton9 = new wxButton( itemPanel12, ID_BUTTON_HTTP_SETTINGS, _("&Settings"),
                                           wxDefaultPosition, wxDefaultSize, 0 );
     itemButton9->SetToolTip(_("Change settings for automatic refresh"));
-    itemBoxSizer5->Add(itemButton9, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    itemBoxSizer5->Add(itemButton9, 0, wxALIGN_CENTER_VERTICAL|wxALL, 4);
     
     //Infobar-mini 
     wxStaticText* itemStaticText44 = new wxStaticText( itemPanel12, ID_PANEL_STOCKS_STATIC_DETAILS_MINI, wxT(""), 
     wxPoint(-1,-1), wxSize(450, -1), wxNO_BORDER|wxST_NO_AUTORESIZE);
-    itemBoxSizer5->Add(itemStaticText44, 1, wxGROW|wxALL, 5);
+    itemBoxSizer5->Add(itemStaticText44, 1, wxGROW|wxALL, 12);
     //Infobar 
     wxStaticText* itemStaticText33 = new wxStaticText( itemPanel12, 
     ID_PANEL_STOCKS_STATIC_DETAILS, _("Using MMEX it is possible to track stocks/mutual funds investments."), 
     wxPoint(-1,-1), wxSize(350, -1), wxNO_BORDER|wxTE_MULTILINE|wxTE_WORDWRAP|wxST_NO_AUTORESIZE);
-    itemBoxSizer4->Add(itemStaticText33, 1, wxGROW|wxALL, 5);
+    itemBoxSizer4->Add(itemStaticText33, 1, wxGROW|wxALL, 1);
     
     //updateExtraStocksData(-1);
 
