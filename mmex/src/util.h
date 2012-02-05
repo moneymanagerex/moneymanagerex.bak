9f6d0397b80ee8609c228c17b6b0c0da3b796551
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-02-05 09:00:40 +0000

    mmex Singleton
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@1730 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/util.h
+++ mmex/src/util.h
@@ -182,6 +182,23 @@ public:
 namespace mmex
 {
 
+template <class T> 
+class Singleton 
+{ 
+public: 
+    static inline T& instance() 
+    { 
+        static T _instance; 
+        return _instance; 
+    } 
+
+private: 
+    Singleton(void); 
+    ~Singleton(void); 
+    Singleton(const Singleton<T>&); 
+    Singleton<T>& operator= (const Singleton<T> &); 
+}; 
+
 class CurrencyFormatter
 {
 public:
