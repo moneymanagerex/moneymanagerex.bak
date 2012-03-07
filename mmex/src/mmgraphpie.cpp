0db5a016a6a88cc273661158757b8fd9ace494dd
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-03-07 15:39:25 +0000

    format
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@1947 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/mmgraphpie.cpp
+++ mmex/src/mmgraphpie.cpp
@@ -4,16 +4,10 @@
 
 #include <algorithm>
 
-namespace
-{
-
 bool sortValueList(const ValuePair& elem1, const ValuePair& elem2)
 {
     return fabs(elem1.amount) > fabs(elem2.amount);
 }
-
-} // namespace
-
 
 mmGraphPie::mmGraphPie() : 
         mmGraphGenerator(PIE),
@@ -27,22 +21,21 @@ mmGraphPie::~mmGraphPie()
 
 void mmGraphPie::init(std::vector<ValuePair>& valueList)
 {
-        std::sort(valueList.begin(), valueList.end(), sortValueList);
-        size_t cnt = std::min(valueList.size(), size_t(15));
+    std::sort(valueList.begin(), valueList.end(), sortValueList);
+    size_t cnt = std::min(valueList.size(), size_t(15));
 
-        std::vector<ChartData> pieData;
-        pieData.reserve(cnt);
+    std::vector<ChartData> pieData;
+    pieData.reserve(cnt);
 
-	for (size_t i = 0; i < cnt; ++i) {
-                pieData.push_back(ChartData(valueList[i].label, valueList[i].amount));
-	}
+    for (size_t i = 0; i < cnt; ++i) 
+        pieData.push_back(ChartData(valueList[i].label, valueList[i].amount));
 
-	chart->SetData(pieData);
-	chart->Init(240, CHART_LEGEND_FIXED, PIE_CHART_ABSOLUT);
+    chart->SetData(pieData);
+    chart->Init(240, CHART_LEGEND_FIXED, PIE_CHART_ABSOLUT);
 }
 
 bool mmGraphPie::Generate(const wxString& chartTitle)
 {
-        chart->Render(chartTitle);
-	return chart->Save(getOutputFileName());
+    chart->Render(chartTitle);
+    return chart->Save(getOutputFileName());
 }
