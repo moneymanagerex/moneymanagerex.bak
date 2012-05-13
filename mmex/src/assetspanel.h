8d1f7c01e816858fa7d8095108b8c3e4b9ecd9b2
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-05-13 13:11:57 +0000

    cleanup
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@2333 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/assetspanel.h
+++ mmex/src/assetspanel.h
@@ -55,9 +55,9 @@ private:
     long m_selectedIndex;
 
     /* required overrides for virtual style list control */
-    virtual wxString OnGetItemText(long item, long column) const;
-    virtual int OnGetItemImage(long item) const;
-    virtual wxListItemAttr *OnGetItemAttr(long item) const;
+    wxString OnGetItemText(long item, long column) const;
+    int OnGetItemImage(long item) const;
+    wxListItemAttr *OnGetItemAttr(long item) const;
 
     void OnItemRightClick(wxListEvent& event);
     void OnListItemActivated(wxListEvent& event);
