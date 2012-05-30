496a0e8dc8f1036b914567f7e82d04087a9be3cc
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-05-30 00:47:14 +0000

    fix budgeting for 2.9.3
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@2458 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/budgetingpanel.cpp
+++ mmex/src/budgetingpanel.cpp
@@ -173,12 +173,14 @@ void mmBudgetingPanel::CreateControls()
     wxBoxSizer* itemBoxSizerHHeader2 = new wxBoxSizer(wxHORIZONTAL);
     itemBoxSizerVHeader->Add(itemBoxSizerHHeader2, 0, wxALL, 1);
 
+#if 0
     wxBitmap itemStaticBitmap3Bitmap(rightarrow_xpm);
     wxStaticBitmap* itemStaticBitmap3 = new wxStaticBitmap( headerPanel, 
         ID_PANEL_BUDGETENTRY_STATIC_BITMAP_VIEW, 
         itemStaticBitmap3Bitmap, wxDefaultPosition, wxSize(16, 16), 0 );
     itemBoxSizerHHeader2->Add(itemStaticBitmap3, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
     itemStaticBitmap3->SetEventHandler( this ); 
+#endif
 
     wxStaticText* itemStaticText18 = new wxStaticText( headerPanel, 
             ID_PANEL_CHECKING_STATIC_PANELVIEW, 
