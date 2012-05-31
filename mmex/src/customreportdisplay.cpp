4b829e1e81fbf4ce10266b1ef2b26deeeff1c507
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-05-31 07:46:57 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@2481 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/customreportdisplay.cpp
+++ mmex/src/customreportdisplay.cpp
@@ -19,7 +19,8 @@
 #include "customreportdisplay.h"
 #include "util.h"
 
-mmCustomSQLReport::mmCustomSQLReport(mmCoreDB* core, const wxString& reportTitle, const wxString& sqlQuery):
+mmCustomSQLReport::mmCustomSQLReport(mmCoreDB* core, const wxString& reportTitle,
+    const wxString& sqlQuery, wxHtmlWindow* htmlWindow):
     mmPrintableBase(core), reportTitle_(reportTitle), sqlQuery_(sqlQuery)
 {
 }
@@ -27,12 +28,7 @@ mmCustomSQLReport::mmCustomSQLReport(mmCoreDB* core, const wxString& reportTitle
 void mmCustomSQLReport::displayReportHeader(mmHTMLBuilder& hb, wxString reportTitle)
 {
     hb.addHeader(2, reportTitle);
-
     hb.addHeaderItalic(0, wxT("Today's Date: ") + mmGetNiceDateString(wxDateTime::Now()));
-    hb.addLineBreak();
-
-	hb.startCenter();
-    hb.startTable(wxT("90%"));
 }
 
 wxString mmCustomSQLReport::getHTMLText()
@@ -40,23 +36,26 @@ wxString mmCustomSQLReport::getHTMLText()
     mmHTMLBuilder hb;
     hb.init();
     displayReportHeader(hb, wxString() << _("Custom SQL Report: ") << reportTitle_ );
+    hb.startCenter();
+    hb.startTable(wxT("90%"));
 
-	wxSQLite3ResultSet sqlQueryResult;
+    wxSQLite3ResultSet sqlQueryResult;
 
     int lines_number;
     wxString error_string;
-	try
-	{
-		lines_number = core_->db_->ExecuteScalar(wxT("select count (*) from (") + sqlQuery_ + wxT(")"));
-	}
-	catch(const wxSQLite3Exception& e)
-	{
-		error_string = e.GetMessage();
-	}
+
+    try
+    {
+        lines_number = core_->db_->ExecuteScalar(wxT("select count (*) from (\n") + sqlQuery_ + wxT("\n)"));
+    }
+    catch(const wxSQLite3Exception& e)
+    {
+        error_string = e.GetMessage();
+    }
 
     wxString lower = error_string.Lower();
-    if (lower.Contains(wxT("update")) || 
-        lower.Contains(wxT("delete")) || 
+    if (lower.Contains(wxT("update")) ||
+        lower.Contains(wxT("delete")) ||
         lower.Contains(wxT("insert")))
     {
         wxMessageDialog msgDlg(0, _("SQL Query will modify your Data. Proceed??"), _("Warning"), wxYES_NO);
@@ -67,15 +66,15 @@ wxString mmCustomSQLReport::getHTMLText()
         }
     }
 
-	try
-	{
-		sqlQueryResult = core_->db_->ExecuteQuery(sqlQuery_);
-	}
-	catch(const wxSQLite3Exception& e)
-	{
-		wxSafeShowMessage(wxT("Error"),e.GetMessage().c_str());
-		return wxString::Format(_("Error: %s"), e.GetMessage().c_str());
-	}
+    try
+    {
+        sqlQueryResult = core_->db_->ExecuteQuery(sqlQuery_);
+    }
+    catch(const wxSQLite3Exception& e)
+    {
+        wxSafeShowMessage(wxT("Error"),e.GetMessage().c_str());
+        return wxString::Format(_("Error: %s"), e.GetMessage().c_str());
+    }
 
     // Use column info to determine if data is to be right justified.
     int columnCount = sqlQueryResult.GetColumnCount();
@@ -84,15 +83,15 @@ wxString mmCustomSQLReport::getHTMLText()
     hb.startTableRow();
     for (int index = 0; index < columnCount; index ++)
     {
-    	hb.addTableHeaderCell(sqlQueryResult.GetColumnName(index));
-    	alignRight[index] = (sqlQueryResult.GetColumnType(index) == WXSQLITE_INTEGER
-    	                  || sqlQueryResult.GetColumnType(index) == WXSQLITE_FLOAT);
+        hb.addTableHeaderCell(sqlQueryResult.GetColumnName(index));
+        alignRight[index] = (sqlQueryResult.GetColumnType(index) == WXSQLITE_INTEGER
+                          || sqlQueryResult.GetColumnType(index) == WXSQLITE_FLOAT);
     }
     hb.endTableRow();
 
     int progress = 0;
     wxString progressMsg;
-    wxProgressDialog dlg(_("Report printing in progress"), progressMsg, lines_number, 
+    wxProgressDialog dlg(_("Report printing in progress"), progressMsg, lines_number,
         NULL, wxPD_AUTO_HIDE |  wxPD_CAN_ABORT);
 
 
@@ -126,9 +125,16 @@ wxString mmCustomSQLReport::getHTMLText()
     sqlQueryResult.Finalize();
 
     hb.endTable();
-	hb.endCenter();
-
+    hb.endCenter();
     hb.end();
+
+    if (lower.Contains(wxT("update")))
+         hb.addHeader(2, _("Dababase updated succesfully"));
+    if (lower.Contains(wxT("delete")))
+         hb.addHeader(2, _("Deletion completed"));
+    if (lower.Contains(wxT("insert")))
+         hb.addHeader(2, _("Data Insertion completed"));
+
     return hb.getHTMLText();
 }
 
