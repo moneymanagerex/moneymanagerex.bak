87f760fd9b29a34ee7453773d16eadac2774e24f
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-06-03 14:22:29 +0000

    tips improvement
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@2510 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/util.cpp
+++ mmex/src/util.cpp
@@ -943,33 +943,33 @@ wxString Tips(wxString type)
     }
     else
     {
-        wxArrayString tips;
-        tips.Add(_("Recommendation: Always backup your .mmb database file regularly."));
-        tips.Add(_("Recommendation: If upgrading to a new version of MMEX, make sure you backup your .mmb database file before doing so."));
-        tips.Add(_("Recommendation: Use copy (Ctrl+С) and paste (Ctrl+V) for frequently used transactions."));
-        tips.Add(_("Tip: Remember to make backups of your .mmb."));
-        tips.Add(_("Tip: The .mmb file is not encrypted. That means anyone else having the proper know how can actually open the file and read the contents. So make sure that if you are storing any sensitive financial information it is properly guarded."));
-        tips.Add(_("Tip: To mark a transaction as reconciled, just select the transaction and hit the 'r' or 'R' key. To mark a transaction as unreconciled, just select the transaction and hit the 'u' or 'U' key."));
-        tips.Add(_("Tip: To mark a transaction as requiring followup, just select the transaction and hit the 'f' or 'F' key."));
-        tips.Add(_("Tip: MMEX supports printing of all reports that can be viewed. The print options are available under the menu, File->Print."));
-        tips.Add(_("Tip: You can modify some runtime behavior of MMEX by changing the options in the Options Dialog. "));
-        tips.Add(_("Tip: To print a statement with transactions from any arbitary set of criteria, use the transaction filter to select the transactions you want and then do a print from the menu."));
-        tips.Add(_("Tip: Set exchange rate for currencies in case if you have accounts with different currencies."));
+        switch (1 + rand() % 21)
+        {
+            case 1 : tipsStr = _("Recommendation: Always backup your .mmb database file regularly."); break;
+            case 2 : tipsStr = _("Recommendation: If upgrading to a new version of MMEX, make sure you backup your .mmb database file before doing so."); break ;
+            case 3 : tipsStr = _("Recommendation: Use copy (Ctrl+С) and paste (Ctrl+V) for frequently used transactions."); break ; break ;
+            case 4 : tipsStr = _("Tip: Remember to make backups of your .mmb."); break ;
+            case 5 : tipsStr = _("Tip: The .mmb file is not encrypted. That means anyone else having the proper know how can actually open the file and read the contents. So make sure that if you are storing any sensitive financial information it is properly guarded."); break ;
+            case 6 : tipsStr = _("Tip: To mark a transaction as reconciled, just select the transaction and hit the 'r' or 'R' key. To mark a transaction as unreconciled, just select the transaction and hit the 'u' or 'U' key."); break ;
+            case 7 : tipsStr = _("Tip: To mark a transaction as requiring followup, just select the transaction and hit the 'f' or 'F' key."); break ;
+            case 8 : tipsStr = _("Tip: MMEX supports printing of all reports that can be viewed. The print options are available under the menu, File->Print."); break ;
+            case 9 : tipsStr = _("Tip: You can modify some runtime behavior of MMEX by changing the options in the Options Dialog. "); break ;
+            case 10 : tipsStr = _("Tip: To print a statement with transactions from any arbitary set of criteria, use the transaction filter to select the transactions you want and then do a print from the menu."); break ;
+            case 11 : tipsStr = _("Tip: Set exchange rate for currencies in case if you have accounts with different currencies."); break ;
 
-        tips.Add(_("Organize Categories Dialog Tip: Pressing the h key will cycle through all categories starting with the letter h"));
-        tips.Add(_("Organize Categories Dialog Tip: Pressing 2 key combination will cycle through all categories starting with that key combination. Example: Pressing ho will select Homeneeds, Home, House Tax, etc..."));
-        tips.Add(_("Organize Payees Dialog Tip: Using the % key as a wildcard when using the filter. Example: %c shows Chemist and Doctor, %c%m shows Chemist only."));
-
-        tips.Add(_("Tip to get out of debt: Pay yourself 10% first. Put this into an account that is hard to touch. Make sure it is a chore to get the money out (you have to drive to the bank), so you will only tap it consciously and for major expenses."));
-        tips.Add(_("Tip to get out of debt: Establish an emergency fund."));
-        tips.Add(_("Tip to get out of debt: Stop acquiring new debt."));
-        tips.Add(_("Tip to get out of debt: Create a realistic budget for your expenses."));
-        tips.Add(_("Tip to get out of debt: Spend less than you earn."));
-        tips.Add(_("Tip to get out of debt: Pay more than the minimum."));
-        tips.Add(_("Before going to a shop and buy something: take the time making a list of what you really need. In the shop buy what is in your list."));
-        tipsStr = tips[rand() % tips.GetCount()];
+            case 12 : tipsStr = _("Organize Categories Dialog Tip: Pressing the h key will cycle through all categories starting with the letter h"); break ;
+            case 13 : tipsStr = _("Organize Categories Dialog Tip: Pressing 2 key combination will cycle through all categories starting with that key combination. Example: Pressing ho will select Homeneeds, Home, House Tax, etc..."); break ;
+            case 14 : tipsStr = _("Organize Payees Dialog Tip: Using the % key as a wildcard when using the filter. Example: %c shows Chemist and Doctor, %c%m shows Chemist only."); break ;
+    
+            case 15 : tipsStr = _("Tip to get out of debt: Pay yourself 10% first. Put this into an account that is hard to touch. Make sure it is a chore to get the money out (you have to drive to the bank), so you will only tap it consciously and for major expenses."); break ;
+            case 16 : tipsStr = _("Tip to get out of debt: Establish an emergency fund."); break ;
+            case 17 : tipsStr = _("Tip to get out of debt: Stop acquiring new debt."); break ;
+            case 18 : tipsStr = _("Tip to get out of debt: Create a realistic budget for your expenses."); break ;
+            case 19 : tipsStr = _("Tip to get out of debt: Spend less than you earn."); break ;
+            case 20 : tipsStr = _("Tip to get out of debt: Pay more than the minimum."); break ;
+            case 21 : tipsStr = _("Before going to a shop and buy something: take the time making a list of what you really need. In the shop buy what is in your list.");
+        }
     }
-
     return tipsStr;
 }
 
