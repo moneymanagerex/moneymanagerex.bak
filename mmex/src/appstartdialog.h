79fbdfa601916dd02834ed67bca94a4e1a378270
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-06-17 17:52:31 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@2627 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/appstartdialog.h
+++ mmex/src/appstartdialog.h
@@ -5,12 +5,12 @@
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -26,7 +26,7 @@ class wxSQLite3Database;
 
 
 class mmAppStartDialog: public wxDialog
-{    
+{
     DECLARE_DYNAMIC_CLASS(mmAppStartDialog)
     DECLARE_EVENT_TABLE()
 
@@ -50,6 +50,7 @@ private:
     void OnButtonAppstartHelpClick( wxCommandEvent& event );
     void OnButtonAppstartWebsiteClick( wxCommandEvent& event );
     void OnButtonAppstartLastDatabaseClick( wxCommandEvent& event );
+    void OnClose( wxCommandEvent& event );
 };
 #endif // _MM_EX_APPSTARTDIALOG_H_
 
