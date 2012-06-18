50cb7063f07612f2df19af4459f11ec4ec64d22b
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-06-18 20:52:46 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@2631 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/assetdialog.cpp
+++ mmex/src/assetdialog.cpp
@@ -3,12 +3,12 @@
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -20,13 +20,13 @@
 #include "defs.h"
 #include "paths.h"
 #include "mmex_db_view.h"
-
+#include <boost/foreach.hpp>
 #include <wx/datectrl.h>
 #define _MM_EX_ASSETDIALOG_CPP_REVISION_ID    "$Revision$"
 enum { DEF_CHANGE_NONE = 0, DEF_CHANGE_APPRECIATE, DEF_CHANGE_DEPRECIATE };
 
-enum 
-{ 
+enum
+{
   IDC_COMBO_TYPE = wxID_HIGHEST + 1,
   IDC_NOTES,
 };
@@ -37,6 +37,7 @@ BEGIN_EVENT_TABLE(mmAssetDialog, wxDialog)
     EVT_BUTTON(wxID_OK, mmAssetDialog::OnOk)
     EVT_BUTTON(wxID_CANCEL, mmAssetDialog::OnCancel)
     EVT_CHOICE(IDC_COMBO_TYPE, mmAssetDialog::OnChangeAppreciationType)
+    EVT_DATE_CHANGED(wxID_ANY, mmAssetDialog::OnDateChanged)
 END_EVENT_TABLE()
 
 
@@ -56,7 +57,7 @@ bool mmAssetDialog::Create(wxWindow* parent,
 {
     SetExtraStyle(GetExtraStyle()|wxWS_EX_BLOCK_EVENTS);
 
-    if (!wxDialog::Create(parent, id, caption, pos, size, style)) 
+    if (!wxDialog::Create(parent, id, caption, pos, size, style))
         return false;
 
     CreateControls();
@@ -64,10 +65,10 @@ bool mmAssetDialog::Create(wxWindow* parent,
     GetSizer()->SetSizeHints(this);
 
     SetIcon(mmex::getProgramIcon());
-    
+
     if (m_edit)
         dataToControls();
-    else   
+    else
         enableDisableRate(false);
 
     Centre();
@@ -84,7 +85,7 @@ void mmAssetDialog::dataToControls()
 
         wxDateTime dtdt = mmGetStorageStringAsDate(asset->STARTDATE);
         wxString dt = mmGetDateForDisplay(m_db, dtdt);
-        m_dpc->SetValue(dtdt);
+        m_dpc_->SetValue(dtdt);
 
         wxString value;
         mmex::formatDoubleToCurrencyEdit(asset->VALUE, value);
@@ -102,47 +103,48 @@ void mmAssetDialog::dataToControls()
 }
 
 void mmAssetDialog::CreateControls()
-{    
-    wxBoxSizer* itemBoxSizer2 = new wxBoxSizer(wxHORIZONTAL);
+{
+    wxSizerFlags flags;
+    flags.Align(wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL).Border(wxALL, 5);
+    //flagsExpand.Align(wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL).Border(wxALL, 5).Expand();
+
+    wxBoxSizer* itemBoxSizer2 = new wxBoxSizer(wxVERTICAL);
     this->SetSizer(itemBoxSizer2);
 
     wxBoxSizer* itemBoxSizer3 = new wxBoxSizer(wxVERTICAL);
-    itemBoxSizer2->Add(itemBoxSizer3, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    itemBoxSizer2->Add(itemBoxSizer3, flags);
 
     wxStaticBox* itemStaticBoxSizer4Static = new wxStaticBox(this, wxID_ANY, _("Asset Details"));
     wxStaticBoxSizer* itemStaticBoxSizer4 = new wxStaticBoxSizer(itemStaticBoxSizer4Static, wxVERTICAL);
-    itemBoxSizer3->Add(itemStaticBoxSizer4, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
+    itemBoxSizer3->Add(itemStaticBoxSizer4, flags);
 
-    wxFlexGridSizer* itemFlexGridSizer6 = new wxFlexGridSizer(8, 2, 0, 0);
-    itemStaticBoxSizer4->Add(itemFlexGridSizer6, 1, wxGROW|wxALL, 5);
+    wxFlexGridSizer* itemFlexGridSizer6 = new wxFlexGridSizer(0, 2, 0, 0);
+    itemStaticBoxSizer4->Add(itemFlexGridSizer6, flags);
 
     wxStaticText* itemStaticText7 = new wxStaticText(this,
-        wxID_STATIC, _("Name"), wxDefaultPosition, wxDefaultSize, 0);
-    itemFlexGridSizer6->Add(itemStaticText7,
-        0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL|wxADJUST_MINSIZE, 5);
+        wxID_STATIC, _("Name"));
+    itemFlexGridSizer6->Add(itemStaticText7, flags);
 
-    m_assetName = new wxTextCtrl(this,
-        wxID_ANY, wxGetEmptyString(), wxDefaultPosition, wxDefaultSize, 0);
+    m_assetName = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(220, -1));
     m_assetName->SetToolTip(_("Enter the name of the asset"));
-    itemFlexGridSizer6->Add(m_assetName, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    itemFlexGridSizer6->Add(m_assetName, flags);
 
     wxStaticText* itemStaticText9 = new wxStaticText(this,
-        wxID_STATIC, _("Date"), wxDefaultPosition, wxDefaultSize, 0);
-    itemFlexGridSizer6->Add(itemStaticText9,
-        0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL|wxADJUST_MINSIZE, 5);
-    
-    m_dpc = new wxDatePickerCtrl(this,
-        wxID_ANY, wxDefaultDateTime, wxDefaultPosition, wxSize(120, -1), wxDP_DROPDOWN|wxDP_SHOWCENTURY);    
-    itemFlexGridSizer6->Add(m_dpc, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
-    m_dpc->SetToolTip(_("Specify the date of purchase of asset"));
+        wxID_STATIC, _("Date"));
+    itemFlexGridSizer6->Add(itemStaticText9, flags);
+
+    m_dpc_ = new wxDatePickerCtrl(this,
+        wxID_ANY, wxDefaultDateTime, wxDefaultPosition, wxSize(130, -1),
+        wxDP_DROPDOWN|wxDP_SHOWCENTURY|wxDP_ALLOWNONE);
+    itemFlexGridSizer6->Add(m_dpc_, flags);
+    m_dpc_->SetToolTip(_("Specify the date of purchase of asset"));
 
     wxStaticText* itemStaticText15 = new wxStaticText(this,
-        wxID_STATIC, _("Asset Type"), wxDefaultPosition, wxDefaultSize, 0);
-    itemFlexGridSizer6->Add(itemStaticText15,
-        0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL|wxADJUST_MINSIZE, 5);
+        wxID_STATIC, _("Asset Type"));
+    itemFlexGridSizer6->Add(itemStaticText15, flags);
 
     m_assetType = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxSize(150, -1));
-    wxString asset_types[] = 
+    wxString asset_types[] =
     {
         wxTRANSLATE("Property"),
         wxTRANSLATE("Automobile"),
@@ -157,26 +159,26 @@ void mmAssetDialog::CreateControls()
 
     m_assetType->SetToolTip(_("Select type of asset"));
     m_assetType->SetSelection(0);
-    itemFlexGridSizer6->Add(m_assetType, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    itemFlexGridSizer6->Add(m_assetType, flags);
 
     wxStaticText* itemStaticText31 = new wxStaticText(this,
-        wxID_STATIC, _("Value"), wxDefaultPosition, wxDefaultSize, 0);
+        wxID_STATIC, _("Value"));
     itemFlexGridSizer6->Add(itemStaticText31,
-        0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL|wxADJUST_MINSIZE, 5);
+        flags);
 
     m_value = new wxTextCtrl(this,
         wxID_ANY, wxGetEmptyString(), wxDefaultPosition, wxSize(150,-1),
         wxALIGN_RIGHT|wxTE_PROCESS_ENTER , wxFloatingPointValidator<float>(2));
     m_value->SetToolTip(_("Enter the current value of the asset"));
-    itemFlexGridSizer6->Add(m_value, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    itemFlexGridSizer6->Add(m_value, flags);
 
     wxStaticText* itemStaticText11 = new wxStaticText(this,
-        wxID_STATIC, _("Change in Value"), wxDefaultPosition, wxDefaultSize, 0);
+        wxID_STATIC, _("Change in Value"));
     itemFlexGridSizer6->Add(itemStaticText11,
-        0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL|wxADJUST_MINSIZE, 5);
+        flags);
 
     m_valueChange = new wxChoice(this, IDC_COMBO_TYPE, wxDefaultPosition, wxSize(150,-1));
-    wxString change_types[] = 
+    wxString change_types[] =
     {
         wxTRANSLATE("None"),
         wxTRANSLATE("Appreciates"),
@@ -184,42 +186,39 @@ void mmAssetDialog::CreateControls()
     };
     for(size_t i = 0; i < sizeof(change_types)/sizeof(wxString); ++i)
         m_valueChange->Append(wxGetTranslation(change_types[i]), new wxStringClientData(change_types[i]));
-        
+
     m_valueChange->SetToolTip(_("Specify if the value of the asset changes over time"));
     m_valueChange->SetSelection(0);
-    itemFlexGridSizer6->Add(m_valueChange, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    itemFlexGridSizer6->Add(m_valueChange, flags);
 
     m_valueChangeRateLabel = new wxStaticText(this,
-        wxID_STATIC, _("% Rate"), wxDefaultPosition, wxDefaultSize, 0);
+        wxID_STATIC, _("% Rate"));
     itemFlexGridSizer6->Add(m_valueChangeRateLabel,
-        0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL|wxADJUST_MINSIZE, 5);
+        flags);
 
     m_valueChangeRate = new wxTextCtrl(this,
         wxID_ANY, wxGetEmptyString(), wxDefaultPosition,
         wxSize(150,-1), wxALIGN_RIGHT|wxTE_PROCESS_ENTER , wxFloatingPointValidator<float>(2));
     m_valueChangeRate->SetToolTip(_("Enter the rate at which the asset changes its value in % per year"));
-    itemFlexGridSizer6->Add(m_valueChangeRate, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    itemFlexGridSizer6->Add(m_valueChangeRate, flags);
     enableDisableRate(false);
 
     wxStaticText* itemStaticText19 = new wxStaticText(this, wxID_STATIC, _("Notes"));
-    itemFlexGridSizer6->Add(itemStaticText19, 0,wxALIGN_LEFT|wxALIGN_LEFT|wxALL|wxADJUST_MINSIZE, 5);
+    itemFlexGridSizer6->Add(itemStaticText19, flags);
 
     m_notes = new wxTextCtrl(this, IDC_NOTES, wxGetEmptyString(), wxDefaultPosition, wxSize(220, 170), wxTE_MULTILINE);
     m_notes->SetToolTip(_("Enter notes associated with this asset"));
-    itemFlexGridSizer6->Add(m_notes, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
-
-    wxPanel* itemPanel27 = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
-    itemBoxSizer3->Add(itemPanel27, 0, wxALIGN_RIGHT|wxALL, 5);
+    itemFlexGridSizer6->Add(m_notes, flags);
 
     wxBoxSizer* itemBoxSizer28 = new wxBoxSizer(wxHORIZONTAL);
-    itemPanel27->SetSizer(itemBoxSizer28);
+    itemBoxSizer2->Add(itemBoxSizer28, flags.Right());
 
-    wxButton* itemButton29 = new wxButton(itemPanel27, wxID_OK);
-    itemBoxSizer28->Add(itemButton29, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
+    wxButton* button_OK = new wxButton(this, wxID_OK);
+    itemBoxSizer28->Add(button_OK, flags);
 
-    wxButton* itemButton30 = new wxButton(itemPanel27, wxID_CANCEL);
-    itemBoxSizer28->Add(itemButton30, 0, wxALIGN_CENTER_VERTICAL|wxALL, 1);
-    itemButton30->SetFocus();
+    wxButton* button_CA = new wxButton(this, wxID_CANCEL);
+    itemBoxSizer28->Add(button_CA, flags);
+    button_CA->SetFocus();
 }
 
 void mmAssetDialog::OnChangeAppreciationType(wxCommandEvent& /*event*/)
@@ -236,7 +235,7 @@ void mmAssetDialog::enableDisableRate(bool en)
         m_valueChangeRate->Enable(true);
         m_valueChangeRateLabel->Enable(true);
     }
-    else 
+    else
     {
         m_valueChangeRate->SetEditable(false);
         m_valueChangeRate->Enable(false);
@@ -246,16 +245,19 @@ void mmAssetDialog::enableDisableRate(bool en)
 
 void mmAssetDialog::OnOk(wxCommandEvent& /*event*/)
 {
-    wxString pdate = m_dpc->GetValue().FormatISODate();
+    wxString pdate = wxDateTime::Now().FormatISODate();
+    if (m_dpc_->GetValue().IsValid())
+        wxString pdate = m_dpc_->GetValue().FormatISODate();
+    else
+    {
+        mmShowErrorMessageInvalid(this, _("Date"));
+        return;
+    }
     wxString notes = m_notes->GetValue().Trim();
     wxString name = m_assetName->GetValue().Trim();
 
     wxString valueStr = m_value->GetValue().Trim();
-    if (valueStr.IsEmpty())
-    {
-        mmShowErrorMessageInvalid(this, _("Value"));
-        return;
-    }
+
     double value = 0;
     if (!mmex::formatCurrencyToDouble(valueStr, value))
     {
@@ -282,6 +284,16 @@ void mmAssetDialog::OnOk(wxCommandEvent& /*event*/)
         return;
     }
 
+    DB_View_ASSETS_V1::Data_Set all_assets = ASSETS_V1.all(m_db);
+    BOOST_FOREACH(const DB_View_ASSETS_V1::Data &assets, all_assets)
+    {
+        wxString asset_name = assets.ASSETNAME.Upper();
+        if (assets.ASSETNAME == m_assetName->GetValue()) {
+            mmShowErrorMessage(this, _("Name already used"),  _("Error"));
+            return;
+        }
+    }
+
     DB_View_ASSETS_V1::Data* asset = ASSETS_V1.get(m_assetID, m_db);
     if (! asset)
         asset = ASSETS_V1.create();
@@ -301,6 +313,14 @@ void mmAssetDialog::OnOk(wxCommandEvent& /*event*/)
     mmOptions::instance().databaseUpdated_ = true;
     EndModal(wxID_OK);
 }
+
+void mmAssetDialog::OnDateChanged(wxDateEvent& event)
+{
+    if (!event.GetDate().IsValid())
+        m_dpc_->SetValue(wxDateTime::Now());
+    event.Skip();
+}
+
 
 void mmAssetDialog::OnCancel(wxCommandEvent& /*event*/)
 {
