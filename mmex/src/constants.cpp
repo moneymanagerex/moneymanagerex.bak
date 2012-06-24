b1bc51f0f25e1eadef3447c2fb036cab3b3be61e
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-06-24 01:01:55 +0000

    Update & clean-up
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@2657 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/constants.cpp
+++ mmex/src/constants.cpp
@@ -32,38 +32,43 @@ wxString mmex::getProgramName()
 
 wxString mmex::getProgramVersion()
 {
-    /******************************************************
-     The commented line has been left to act as a template
-     for when a development version is created for release.
-     ******************************************************/
-    //return wxString(("0.9.9.0 DEV:SVN-xxxx"));
-    return wxString::Format(("0.10.0.0 %s"), _MM_EX_REVISION_ID);
+    wxString revision(_MM_EX_REVISION_ID); 
+    revision.Replace('$',wxEmptyString);
+
+    /**************************************************
+     comment out this line for a release
+     revision.Replace("Rev: ","DEV:SVN-");
+     **************************************************/
+    revision.Replace("Rev: ","DEV:SVN-");
+
+    revision.Trim();
+    return wxString::Format("0.10.0.0  %s", revision);
 }
 
 wxString mmex::getProgramCopyright()
 {
-    return ("(c) 2005-2012 Madhan Kanagavel");
+    return "(c) 2005-2012 Madhan Kanagavel";
 }
 
 wxString mmex::getProgramWebSite()
 {
-    return ("http://codelathe.com/mmex");
+    return "http://codelathe.com/mmex";
 }
 
 wxString mmex::getProgramFacebookSite()
 {
-    return ("http://www.facebook.com/pages/Money-Manager-Ex/242286559144586");
+    return "http://www.facebook.com/pages/Money-Manager-Ex/242286559144586";
 }
 
 wxString mmex::getProgramDescription()
 {
     wxString description = _("Released under the GNU GPL License");
-    description << ("\n\n")
+    description << "\n\n"
                 << wxVERSION_STRING
-                << (",  Boost C++ ") << (BOOST_VERSION/100000) << ('.')
-                << (BOOST_VERSION / 100 % 1000) << ('.')
+                << ",  Boost C++ " << (BOOST_VERSION/100000) << '.'
+                << (BOOST_VERSION / 100 % 1000) << '.'
                 << (BOOST_VERSION % 100) << (",\n")
-                << ("SQLite3 ") << wxSQLite3Database::GetVersion()
-                << (",  wxSQLite 3.0.0");
+                << "SQLite3 " << wxSQLite3Database::GetVersion()
+                << ",  wxSQLite 3.0.0";
     return description;
 }
