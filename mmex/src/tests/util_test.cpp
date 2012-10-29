8dde2c7b5ab5b379ad6707a49755cccde824aba7
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-10-29 00:47:00 +0000

    Update
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@3101 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/tests/util_test.cpp
+++ mmex/src/tests/util_test.cpp
@@ -19,7 +19,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //----------------------------------------------------------------------------
 #include <UnitTest++.h>
 #include "util.h"
-#include "utils.h"
 //----------------------------------------------------------------------------
 #include <boost/unordered_map.hpp>
 #include <wx/log.h>
