27fa7847cdf7ae2da43b4d97eedfa2e5714eeea2
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2012-11-04 11:36:52 +0000

    Update
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@3105 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/tests/dbwrapper_test.cpp
+++ mmex/src/tests/dbwrapper_test.cpp
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <wx/filename.h>
 //----------------------------------------------------------------------------
 #include "dbwrapper.h"
-#include "utils.h"
 #include "mmcoredb.h"
 #include "mmex_settings.h"
 #include <iostream>
