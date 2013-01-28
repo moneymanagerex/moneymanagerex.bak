692498dd9daf6b5d0c5e1d003b63da375f6b6149
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-01-28 14:20:58 +0000

    fix for last used category
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@3402 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/mmtransaction.cpp
+++ mmex/src/mmtransaction.cpp
@@ -855,25 +855,42 @@ wxDateTime mmBankTransactionList::getLastDate(int accountID) const
     return dt;
 }
 
-int mmBankTransactionList::getTransferCategoryID(const int accountID, int& subcategID) const
+int mmBankTransactionList::getLastUsedCategoryID(const int accountID, const int payeeID, int& subcategID) const
 {
     int categ_id = -1;
     subcategID = -1;
     int index = transactions_.size() - 1;
     bool searching = true;
+    bool transfer = (accountID > 0 && payeeID < 0);
     while (searching && index >= 0)
     {
         boost::shared_ptr<const mmBankTransaction> pTransaction = transactions_[index];
         if (pTransaction)
         {
-            if ((pTransaction->accountID_ == accountID || pTransaction->toAccountID_ == accountID)
-                && pTransaction->transType_ == TRANS_TYPE_TRANSFER_STR)
+            if (transfer)
             {
-                categ_id = pTransaction->categID_;
-                if (categ_id > -1)
+                if ((pTransaction->accountID_ == accountID || pTransaction->toAccountID_ == accountID)
+                    && pTransaction->transType_ == TRANS_TYPE_TRANSFER_STR)
                 {
-                    subcategID = pTransaction->subcategID_;
-                    searching = false;
+                    categ_id = pTransaction->categID_;
+                    if (categ_id > -1)
+                    {
+                        subcategID = pTransaction->subcategID_;
+                        searching = false;
+                    }
+                }
+            }
+            else
+            {
+                if (pTransaction->payeeID_ == payeeID
+                    && pTransaction->transType_ != TRANS_TYPE_TRANSFER_STR)
+                {
+                    categ_id = pTransaction->categID_;
+                    if (categ_id > -1)
+                    {
+                        subcategID = pTransaction->subcategID_;
+                        searching = false;
+                    }
                 }
             }
         }
