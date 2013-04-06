49a2f091f694be5ff74a1247da0f2e7f4f86701e
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-04-06 00:33:26 +0000

    Property change: eol-style = native Force revision counter update in constants.cpp
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@3650 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/constants.cpp
+++ mmex/src/constants.cpp
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  Commit a change to the SVN repository, then create the build.
  *************************************************************************/
 const wxString MMEX_REVISION_ID = wxT("$Rev$");
-//----------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 const int mmex::MIN_DATAVERSION = 2;
 const wxChar * const mmex::DATAVERSION = wxT("2");
 const wxChar *const mmex::DEFDATEFORMAT = wxT("%m/%d/%y");
