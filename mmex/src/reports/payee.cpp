dd922c5811806a07ff5d762e97c0b53e7f609066
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-04-28 18:30:46 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@3795 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/reports/payee.cpp
+++ mmex/src/reports/payee.cpp
@@ -21,10 +21,8 @@ wxString mmReportPayeeExpenses::getHTMLText()
     mmHTMLBuilder hb;
     hb.init();
     hb.addHeader(2, title_);
-
-    hb.DisplayDateHeading(date_range_->start_date(), date_range_->end_date(), date_range_->is_with_date());   
-
-	hb.startCenter();
+    hb.DisplayDateHeading(date_range_->start_date(), date_range_->end_date(), date_range_->is_with_date());
+    hb.startCenter();
 
     // Add the graph
     mmGraphPie gg;
@@ -32,74 +30,61 @@ wxString mmReportPayeeExpenses::getHTMLText()
     {
         hb.addImage(gg.getOutputFileName());
     }
-	hb.startTable("50%");
-	hb.startTableRow();
-	hb.addTableHeaderCell(_("Payee"));
-	hb.addTableHeaderCell(_("Amount"), true);
-	hb.endTableRow();
+
+    hb.startTable("50%");
+    hb.startTableRow();
+    hb.addTableHeaderCell(_("Payee"));
+    hb.addTableHeaderCell(_("Amount"), true);
+    hb.endTableRow();
 
     core_->currencyList_.LoadBaseCurrencySettings();
-        
-    double total = 0.0;
-    double positiveTotal = 0.0;
-    double negativeTotal = 0.0;
+
+    double total = 0.0, positiveTotal = 0.0, negativeTotal = 0.0;
     std::vector<ValuePair> valueList;
-    std::pair<mmPayeeList::const_iterator, mmPayeeList::const_iterator> payee_range = core_->payeeList_.Range();
-    for (mmPayeeList::const_iterator it = payee_range.first; it != payee_range.second; ++ it)
+    bool ignore_date = !date_range_->is_with_date();
+
+    for (const auto& payee: core_->payeeList_.entries_)
     {
-        double amt = core_->bTransactionList_.getAmountForPayee((*it)->id_,
-            date_range_->is_with_date(), date_range_->start_date(), date_range_->end_date(), mmIniOptions::instance().ignoreFutureTransactions_
-        );
+        double amt = core_->bTransactionList_.getAmountForPayee(payee->id_
+            , ignore_date, date_range_->start_date()
+            , date_range_->end_date(), mmIniOptions::instance().ignoreFutureTransactions_);
 
         if (amt != 0.0)
         {
             total += amt;
             if (amt>0.0)
-            {
                 positiveTotal += amt;
-            }
-            if (amt<0.0)
-            {
+            else
                 negativeTotal += amt;
-            }
             ValuePair vp;
-            vp.label = (*it)->name_;
+            vp.label = payee->name_;
             vp.amount = amt;
             valueList.push_back(vp);
 
             hb.startTableRow();
-            hb.addTableCell((*it)->name_, false, true);
-
+            hb.addTableCell(payee->name_, false, true);
             hb.addMoneyCell(amt);
             hb.endTableRow();
-		}
+        }
     }
 
-    wxString payeetotalStr;
-    wxString positiveTotalStr;
-    wxString negativeTotalStr;
-    wxString colorStr = (total<0.0 ? "RED" : "BLACK");
-    mmex::formatDoubleToCurrency(total, payeetotalStr);
-    mmex::formatDoubleToCurrency(positiveTotal, positiveTotalStr);
-    mmex::formatDoubleToCurrency(negativeTotal, negativeTotalStr);
-
     hb.addRowSeparator(2);
     hb.startTableRow();
-    hb.addTableCell(_("Income:"),false, true, true, "BLACK");
-    hb.addTableCell(positiveTotalStr, true, false, true, "BLACK");
-	hb.endTableRow();
+    hb.addTableCell(_("Income:"), false, true, true);
+    hb.addMoneyCell(positiveTotal);
+    hb.endTableRow();
     hb.startTableRow();
-    hb.addTableCell(_("Expenses:"),false, true, true, "BLACK");
-    hb.addTableCell(negativeTotalStr, true, false, true, "RED");
-	hb.endTableRow();
+    hb.addTableCell(_("Expenses:"), false, true, true);
+    hb.addMoneyCell(negativeTotal);
+    hb.endTableRow();
     hb.addRowSeparator(2);
     hb.startTableRow();
-    hb.addTableCell(_("Payees Total: "),false, true, true, "BLACK");
-    hb.addTableCell(payeetotalStr, true, false, true, colorStr);
-	hb.endTableRow();
+    hb.addTableCell(_("Payees Total:"), false, true, true);
+    hb.addMoneyCell(total);
+    hb.endTableRow();
 
     hb.endTable();
-	hb.endCenter();
+    hb.endCenter();
 
     hb.end();
 
