36acdae0d30f41a9942b271023d8f7da316cd578
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-05-04 07:12:54 +0000

    mmIniOptions
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@3831 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/util.cpp
+++ mmex/src/util.cpp
@@ -155,43 +155,6 @@ bool mmex::formatCurrencyToDouble(const wxString& str, double& val)
     return wxNumberFormatter::FromString(str , &val);
 }
 
-//----------------------------------------------------------------------------
-mmIniOptions::mmIniOptions()
-: enableAssets_(true)
-, enableBudget_(true)
-, enableGraphs_(true)
-, enableAddAccount_(true)
-, enableDeleteAccount_(true)
-, enableRepeatingTransactions_(true)
-, enableCustomLogo_(false)
-, enableCheckForUpdates_(true)
-, enableReportIssues_ (true)
-, enableBeNotifiedForNewReleases_(true)
-, enableVisitWebsite_(true)
-, html_font_size_(3)
-, enableCustomAboutDialog_(false)
-, disableCategoryModify_(false)
-, enableCustomTemplateDB_(false)
-, expandBankHome_(true)
-, expandTermHome_(false)
-, expandStocksHome_(true)
-, expandBankTree_(true)
-, expandTermTree_(false)
-, budgetFinancialYears_(false)
-, budgetIncludeTransfers_(false)
-, budgetSetupWithoutSummaries_(false)
-, budgetSummaryWithoutCategories_(true)
-, ignoreFutureTransactions_(false)
-, transPayeeSelectionNone_(0)
-, transCategorySelectionNone_(0)
-, transStatusReconciled_(0)
-, transDateDefault_(0)
-{}
-
-mmIniOptions& mmIniOptions::instance()
-{
-    return Singleton<mmIniOptions>::instance();
-}
 
 //----------------------------------------------------------------------------
 void correctEmptyFileExt(wxString ext, wxString & fileName)
