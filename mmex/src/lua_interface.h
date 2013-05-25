4e3505ef319da53f4c851c73fb81eaa22f401ae7
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-05-25 16:17:02 +0000

    new Lua function mmHTMLTableCellMonth
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@3985 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/lua_interface.h
+++ mmex/src/lua_interface.h
@@ -81,6 +81,7 @@ private:
     static int cpp2lua_GetExeDir(lua_State* lua);
     static int cpp2lua_GetLuaDir(lua_State* lua);
     static int cpp2lua_HTMLBuilder(lua_State* lua);
+    static int cpp2lua_HTMLTableCellMonth(lua_State* lua);
 };
 
 #endif // _MM_EX_LUA_INTERFACE_H_
