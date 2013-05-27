d841c484b43eb659eb3c7ec76ce623bf289b472e
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-05-27 13:22:45 +0000

    more Lua html functions
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4001 7f5d68d0-c712-0410-839f-9b116a576cc5

--- mmex/src/scripts/transaction_stats.lua
+++ mmex/src/scripts/transaction_stats.lua
@@ -1,34 +1,27 @@
-local function add_header () 
-   mmHTMLStartTableRow() 
-      for i = -1, 10 - 1, 1 do 
-          local y = 2013 - i 
-          if i == -1 then y = nil end 
-          mmHTMLBuilder("AddTableHeaderCell", y) 
-      end 
-   mmHTMLEndTableRow() 
-end 
 local function get_stats()
     --test table
-	m_stat = {} -- TODO: provide c++ function to get stats
-	y_stat = {1,2,3,4,5,6,7,8,9,10}
-	for m = 1, 12, 1 do m_stat[m] = (y_stat) end
-	--test end
+    m_stat = {} -- TODO: provide c++ function to get stats
+    y_stat = {1,2,3,4,5,6,7,8,9,10}
+    for m = 1, 12, 1 do m_stat[m] = (y_stat) end
+    --test end
 
-	for m = 1, 12 , 1 do 
-		mmHTMLStartTableRow() 
-		mmHTMLTableCellMonth(m) 
-		for y = 1, 10, 1 do 
-			local c = m_stat[m][y]
-			mmHTMLTableCellInteger(c) 
-		end 
-		mmHTMLEndTableRow() 
-	end 
-end 
------------------------------------------------------------ 
---Main function 
------------------------------------------------------------  
-mmHTMLReportHeader(_("Transaction Statistics")) 
-mmHTMLStartTable("50%") 
-add_header() 
-get_stats() 
-mmHTMLEndTable() 
+    for m = 0, 12 , 1 do
+        mmHTMLStartTableRow()
+        mmHTMLTableCellMonth(m)
+        for y = 1, 10, 1 do
+            if m > 0 then
+                mmHTMLTableCellInteger(m_stat[m][y])
+            else
+                mmHTMLTableHeaderCell(y)
+            end
+        end
+        mmHTMLEndTableRow()
+    end
+end
+-----------------------------------------------------------
+--Main function
+-----------------------------------------------------------
+mmHTMLReportHeader(_("Transaction Statistics"))
+mmHTMLStartTable("50%")
+get_stats()
+mmHTMLEndTable()
